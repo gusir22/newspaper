746785d474dd8c09b32a2b615cd7b454ce7fa0bd
Gustavo <gusir22@gmail.com>
2023-06-24 19:47:47 -0400

    added bootstap and crispy forms styling

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd Party
+    'crispy_forms',
+
+    # Local
     'accounts',
     'pages',
 ]
@@ -125,3 +130,6 @@ STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# Crispy Forms 3rd Party App
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
