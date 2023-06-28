7685f323fff18ecff6ba000d3aba1da0caecfab3
Gustavo <gusir22@gmail.com>
2023-06-27 21:27:43 -0400

    added Comments model and Article authors are only ones to see edit or delete buttons

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
 
 
-admin.site.register(Article)
+class CommentInLine(admin.TabularInline):
+    model = Comment
+    extra = 0  # tells django to not add the default 3 extra inlines
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInLine,
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
--- articles/models.py
+++ articles/models.py
@@ -22,3 +22,19 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         return reverse('article_detail', args=[str(self.id)])
+
+    def body_preview(self):
+        """Returns the first 200 chars of the article body to display a preview in the ArticleListView"""
+        return f"{self.body[:200]}..."
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+    comment = models.CharField(max_length=140)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comment
+
+    def get_absolute_url(self):
+        return reverse('article_detail', args=[str(self.article.id)])
